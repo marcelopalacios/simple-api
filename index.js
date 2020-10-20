db106b01f078f304b92a38ab19f0bae0b18238c3
Marcelo Palacios Mora <mpalaciosmora@gmail.com>
2020-10-19 23:49:14 -0300

    commit v2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+ path = require('path');
 var app = express(),
 cors = require("cors"),
 bodyParser = require("body-parser");
@@ -14,9 +15,15 @@ const usersRouter = require('./routes/users');
 //usar los modulos
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 //use router
 app.use('/users',usersRouter);
+
+// router user input
+app.get('/', function(req, res) {
+res.sendFile(path.resolve(__dirname,'views') + '/index.html');
+});
 
 //iniciando el servidor
 app.listen( server.port, () => console.log(`Server started, listening port: ${server.port}`));
